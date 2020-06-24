884075620836a6e672d70c7ac9173224d553cd78
yaozhengwei <cto.china@qq.com>
2020-06-24 18:12:20 +0800

    chore:工作流组件完善

--- src/components/yWorkFDesign/Index.vue
+++ src/components/yWorkFDesign/Index.vue
@@ -11,14 +11,15 @@
         <el-button type="danger">返回</el-button>
       </div>
     </div>
-    <div id="canvas" class="disabledUpdateText" @contextmenu.stop.prevent="$refs.ctxshow.showMenu($event,$event)"></div>
+    <div id="canvas" class="disabledUpdateText" @click="canvasClick" @contextmenu.stop.prevent="$refs.ctxshow.showMenu($event,$event)"></div>
     <e-vue-contextmenu ref="ctxshow" id="contextStyle" @ctx-show="show" @ctx-hide="hide">
       <ul>
         <li v-show="rcContentMenus.indexOf('btn1')>-1" data-id="btn1" @click="save"><i class="el-icon-success"></i>&nbsp;<span>保存设计</span></li>
         <li v-show="rcContentMenus.indexOf('btn2')>-1" data-id="btn2" @click="addUserTask"><i class="el-icon-circle-plus"></i>&nbsp;<span>添加用户步骤</span></li>
-        <li v-show="rcContentMenus.indexOf('btn4')>-1" data-id="btn4" @click="edit"><i class="el-icon-setting"></i>&nbsp;<span>属性</span></li>
+        <li v-show="rcContentMenus.indexOf('btn4')>-1" data-id="btn4" @click="editTask"><i class="el-icon-setting"></i>&nbsp;<span>属性</span></li>
         <li v-show="rcContentMenus.indexOf('btn5')>-1" data-id="btn5" @click="deleteTask"><i class="el-icon-delete"></i>&nbsp;<span>删除</span></li>
-        <li v-show="rcContentMenus.indexOf('btn6')>-1" data-id="btn5" @click="connectLine"><i class="el-icon-share"></i>&nbsp;<span>连线</span></li>
+        <li v-show="rcContentMenus.indexOf('btn6')>-1" data-id="btn6" @click="connectLine"><i class="el-icon-share"></i>&nbsp;<span>连线</span></li>
+        <li v-show="rcContentMenus.indexOf('btn7')>-1" data-id="btn7" @click="resetDesign"><i class="el-icon-refresh"></i>&nbsp;<span>重置</span></li>
       </ul>
     </e-vue-contextmenu>
     <el-dialog
@@ -26,27 +27,27 @@
       center
       :show-close="false">
       <el-form :model="taskForm">
-        <el-form-item label="步骤名称" label-width="200px">
-          <el-input autocomplete="off"></el-input>
+        <el-form-item label="步骤名称" label-width="100px">
+          <el-input autocomplete="off" v-model="taskForm.name"></el-input>
         </el-form-item>
-        <el-form-item label="用户类别" label-width="200px">
+        <el-form-item label="用户类别" label-width="100px">
           <el-radio-group>
             <el-radio-button label="人员"></el-radio-button>
             <el-radio-button label="岗位"></el-radio-button>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="用户名称" label-width="200px">
+        <el-form-item label="用户名称" label-width="100px">
           <el-input placeholder="请选择用户名称" class="input-with-select" :disabled="true">
             <el-button slot="append" type="primary">选择</el-button>
           </el-input>
         </el-form-item>
-        <el-form-item label="协作" label-width="200px">
+        <el-form-item label="协作" label-width="100px">
           <el-radio label="1">竞争</el-radio>
           <el-radio label="2">会签</el-radio>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        <el-button type="primary" @click="saveTask">确 定</el-button>
         <el-button @click="dialogFormVisible = false">取 消</el-button>
       </div>
     </el-dialog>
@@ -73,9 +74,11 @@
         contextMenuVisible:true,
         rcContentMenus:[],
         currentClickTask:null,
-        currentClickEle:null,
+        currentClickEvent:null,
         dialogFormVisible:false,
-        taskForm:{}
+        taskForm:{
+          name:''
+        }
       }
     },
     mounted(){
@@ -88,65 +91,52 @@
       //如果接收id不为空，请求服务器数据
       if (that.id){
         that.$api.workflow.getDetail().then(data => {
-          that.loading = false;
-          that.bpmnModeler.importXML(data, function(err) {
-            if (!err) {
-              // const elementRegistry = that.bpmnModeler.get('elementRegistry');
-              // that.nodeList = elementRegistry.filter (
-              //   (item) => item.type === 'bpmn:UserTask' || item.type === 'bpmn:ServiceTask'
-              // );
-              // let modeling = that.bpmnModeler.get('modeling');
-              // modeling.setColor(that.nodeList[0], {
-              //   stroke: 'green',
-              //   fill: 'yellow'
-              // });
-              that.addEventBusListener();
-            }else{
-              that.$message.error('初始化数据错误');
-            }
-          })
+          that.initDesign(data);
         })
       }else{
         //重置
-        that.resetDesign();
+        that.initDesign(xmlStr);
       }
     },
     methods: {
       addEventBusListener() {
         // 监听 element
-        let that = this
-        const eventBus = this.bpmnModeler.get('eventBus')
-        const modeling = this.bpmnModeler.get('modeling')
-        const elementRegistry = this.bpmnModeler.get('elementRegistry')
-        const eventTypes = ['element.changed','element.dblclick']
+        let that = this;
+        const eventBus = this.bpmnModeler.get('eventBus');
+        const modeling = this.bpmnModeler.get('modeling');
+        const elementRegistry = this.bpmnModeler.get('elementRegistry');
+        const eventTypes = ['element.dblclick'];
         eventTypes.forEach(function(eventType) {
           eventBus.on(eventType, function(e) {
             if (!e || e.element.type == 'bpmn:Process'){
               return false;
             }
             if (eventType === 'element.dblclick'){
-              if (e.element.type == 'bpmn:StartEvent') {
-                that.$alert('这是开始节点', '提示', {
-                  confirmButtonText: '确定',
-                  center: true
-                });
-              }
-              if (e.element.type == 'bpmn:EndEvent') {
-                that.$alert('这是结束节点', '提示', {
-                  confirmButtonText: '确定',
-                  center: true
-                });
-              }
-              if (e.element.type == 'bpmn:UserTask') {
-                that.dialogFormVisible = true;
+              switch (e.element.type) {
+                case 'bpmn:StartEvent':
+                  that.$alert('这是开始节点', '提示', {
+                    confirmButtonText: '确定',
+                    center: true
+                  });
+                  break;
+                case 'bpmn:EndEvent':
+                  that.$alert('这是结束节点', '提示', {
+                    confirmButtonText: '确定',
+                    center: true
+                  });
+                  break;
+                case 'bpmn:UserTask':
+                  that.currentClickTask = e.element;
+                  that.editTask();
+                  break;
               }
             }
           })
         })
       },
-      resetDesign() {
+      initDesign(xmlData) {
         const that = this;
-        that.bpmnModeler.importXML(xmlStr, function(err) {
+        that.bpmnModeler.importXML(xmlData, function(err) {
           that.loading = false;
           if (err) {
             that.$message.error('初始化错误');
@@ -156,51 +146,77 @@
           }
         })
       },
+      resetDesign() {
+        const that = this;
+        that.$refs.ctxshow.hideMenu();
+        that.$confirm('此操作将重置该流程设计, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          center:true
+        }).then(() => {
+          that.initDesign(xmlStr);
+        }).catch(() => {
+        });
+      },
       addUserTask(e) {
         this.$refs.ctxshow.hideMenu();
         if (!this.currentClickTask) {
           return;
         }
+        const modeling = this.bpmnModeler.get('modeling');
         const elementFactory = this.bpmnModeler.get('elementFactory');
         const autoPlace = this.bpmnModeler.get('autoPlace');
         const shape = elementFactory.createShape({ type: 'bpmn:UserTask' });
+        modeling.setColor(shape, {
+          fill: 'yellow'
+        });
+        this.initAllTaskStyle();
         autoPlace.append(this.currentClickTask, shape);
+
+        let mouseEvents = document.createEvent('MouseEvents');
+        mouseEvents.initEvent("click", false, true);
+        document.body.querySelector("#canvas").dispatchEvent(mouseEvents);
+      },
+      selectedTask(htmlEle){
+        const that = this;
+        let modeling = this.bpmnModeler.get('modeling');
+        that.initAllTaskStyle();
+        let taskId = that.findCurrentClickDomTask(htmlEle);
+        if (taskId) {
+          const elementRegistry = that.bpmnModeler.get('elementRegistry');
+          that.currentClickTask = elementRegistry.get(taskId);
+          modeling.setColor(this.currentClickTask, {
+            fill: 'yellow'
+          });
+
+          return that.currentClickTask;
+        }else{
+          return null;
+        }
       },
       show(e) {
         this.hide();
-        this.currentClickEle = e;
-        if (e && e.target && e.target.parentNode){
-          let taskId = e.target.parentNode;
-          if (taskId) {
-            taskId = taskId.getAttribute('data-element-id');
-            if (!taskId){
-              taskId = e.target.parentNode.parentNode.parentNode;
-              if (taskId) {
-                taskId = taskId.getAttribute('data-element-id');
-              }
-            }
-            if (taskId) {
-              const elementRegistry = this.bpmnModeler.get('elementRegistry');
-              this.currentClickTask = elementRegistry.get(taskId);
-              if (this.currentClickTask.type === 'bpmn:EndEvent') {
-                this.$refs.ctxshow.hideMenu();
-              }else{
-                if (this.currentClickTask.type === 'bpmn:StartEvent') {
-                  this.rcContentMenus = ['btn2','btn6'];
-                }else if(this.currentClickTask.type === 'bpmn:SequenceFlow'){
-                  this.rcContentMenus = ['btn5'];
-                }else{
-                  this.rcContentMenus = ['btn2','btn4','btn5','btn6'];
-                }
-              }
+        this.currentClickEvent = e;
+        if (e && e.target){
+          let currTask = this.selectedTask(e.target);
+          if (currTask) {
+            if (currTask.type === 'bpmn:EndEvent') {
+              this.$refs.ctxshow.hideMenu();
             }else{
-              this.rcContentMenus = ['btn1'];
+              if (currTask.type === 'bpmn:StartEvent') {
+                this.rcContentMenus = ['btn2','btn6'];
+              }else if(currTask.type === 'bpmn:SequenceFlow'){
+                this.rcContentMenus = ['btn5'];
+              }else{
+                this.rcContentMenus = ['btn2','btn4','btn5','btn6'];
+              }
             }
           }else{
-            this.rcContentMenus = ['btn1'];
+            this.rcContentMenus = ['btn1','btn7'];
           }
         }else{
-          this.rcContentMenus = ['btn1'];
+          this.rcContentMenus = ['btn1','btn7'];
         }
       },
       hide() {
@@ -209,20 +225,28 @@
       deleteTask(){
         const that = this;
         that.$refs.ctxshow.hideMenu();
-        if (that.currentClickTask) {
-          if (that.currentClickTask.type === 'bpmn:StartEvent') {
-            that.$message.error('开始节点不能删除');
-            return;
+        that.$confirm('请确认是否删除该节点?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          center:true
+        }).then(() => {
+          if (that.currentClickTask) {
+            if (that.currentClickTask.type === 'bpmn:StartEvent') {
+              that.$message.error('开始节点不能删除');
+              return;
+            }
+            if (that.currentClickTask.type === 'bpmn:EndEvent') {
+              that.$message.error('结束节点不能删除');
+              return;
+            }
+            const modeling = that.bpmnModeler.get('modeling');
+            modeling.removeElements([that.currentClickTask]);
+          }else{
+            that.$message.error('你没有选中任何节点');
           }
-          if (that.currentClickTask.type === 'bpmn:EndEvent') {
-            that.$message.error('结束节点不能删除');
-            return;
-          }
-          const modeling = that.bpmnModeler.get('modeling');
-          modeling.removeElements([that.currentClickTask]);
-        }else{
-          that.$message.error('你没有选中任何节点');
-        }
+        }).catch(() => {
+        });
       },
       save(){
         const that = this;
@@ -238,9 +262,11 @@
           return;
         }
         const connect = this.bpmnModeler.get('connect');
-        connect.start(this.currentClickEle,this.currentClickTask);
+        connect.start(this.currentClickEvent,this.currentClickTask);
       },
-      edit(){
+      editTask(data){
+        this.$refs.ctxshow.hideMenu();
+        this.taskForm.name = this.currentClickTask.businessObject.name;
         this.dialogFormVisible = true;
       },
       // 下载为bpmn格式,done是个函数，调用的时候传入的
@@ -249,6 +275,51 @@
         this.bpmnModeler.saveXML({ format: true }, function(err, xml) {
           done(err, xml)
         })
+      },
+      saveTask(){
+        const that = this;
+        const modeling = that.bpmnModeler.get('modeling');
+        modeling.updateProperties(that.currentClickTask, {
+          name: that.taskForm.name
+        })
+        that.dialogFormVisible = false;
+      },
+      findCurrentClickDomTask(e){
+        let isElement =function (obj) {
+          return !!(obj && (obj.nodeType == 1)); //判断是否是Element元素
+        }
+        if (isElement(e)){
+          let taskId = e.getAttribute('data-element-id');
+          if (taskId && taskId !== 'Process_1') {
+            return taskId;
+          }else {
+            if (e.parentNode){
+              return this.findCurrentClickDomTask(e.parentNode);
+            }else{
+              return null;
+            }
+          }
+        }
+        return null;
+      },
+      canvasClick(e){
+        const that = this;
+        if (e && e.target){
+          let currTask = that.selectedTask(e.target);
+        }
+      },
+      initAllTaskStyle(){
+        const that = this;
+        const elementRegistry = that.bpmnModeler.get('elementRegistry');
+        let nodeList = elementRegistry.filter (
+          (item) => item.type === 'bpmn:UserTask'
+        );
+        let modeling = that.bpmnModeler.get('modeling');
+        if (nodeList && nodeList.length > 0) {
+          modeling.setColor(nodeList, {
+            fill: 'white'
+          });
+        }
       }
     }
   }
