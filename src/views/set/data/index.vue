3bb989679d59d86a522f283de55828cd3530b1fd
caoxiangxiang <1104729815@qq.com>
2020-07-17 16:54:27 +0800

    ui

--- src/views/set/data/index.vue
+++ src/views/set/data/index.vue
@@ -1,130 +1,124 @@
 <template>
   <div class="app-container">
     <div class="title-container">
-      <!--      <breadcrumb id="breadcrumb-container" class="breadcrumb-container" style="float: left"/>-->
-
-      <!--      <el-button class="filter-item download-button" type="primary" icon="el-icon-edit">-->
-      <!--        导出-->
-      <!--      </el-button>-->
-      <div class="filter-container" style="float: left;margin-top: 10px;">
-        <span class="filter-item">类型列表:</span>
-        <el-select v-model="value" clearable filterable placeholder="请选择" class="filter-item" @change="selectChange">
-          <el-option
-            v-for="item in options"
-            :key="item.key"
-            :label="item.name"
-            :value="item.key">
-          </el-option>
-        </el-select>
-        <el-button v-if="value" class="filter-item" icon="el-icon-plus" style="margin-left: 10px;" type="primary" @click="handleAdd">
-          新增类型
-        </el-button>
+      <breadcrumb id="breadcrumb-container" class="breadcrumb-container" style="float: left" />
+    </div>
+    <div class="analysis">
+      <div class="menu-2-box">
+        <!--        <div-->
+        <!--          :key="index"-->
+        <!--          class="menu-2-item hvr-underline-from-center"-->
+        <!--        >-->
+        <!--          <i class="easy-icon easy-icon-avatar" />-->
+        <!--          <div class="text">-->
+        <!--            <div class="analysis-text">12000</div>-->
+        <!--            <div class="analysis-text-small">在读学生总数</div>-->
+        <!--          </div>-->
+        <!--        </div>-->
+        <!--        <div-->
+        <!--          :key="index"-->
+        <!--          class="menu-2-item hvr-underline-from-center"-->
+        <!--        >-->
+        <!--          <i class="easy-icon easy-icon-avatar" />-->
+        <!--          <div class="text">-->
+        <!--            <div class="analysis-text">11：9</div>-->
+        <!--            <div class="analysis-text-small">在读学生男女比例</div>-->
+        <!--          </div>-->
+        <!--        </div>-->
       </div>
-      <div class="filter-container" style="float: right;margin-top: 10px;">
+    </div>
+    <div class="filter-main-div">
+      <div class="filter-container" style="width:70%; float: left;">
+        <div>
+          <span class="filter-item">类型列表:</span>
+          <el-select v-model="value" clearable filterable placeholder="请选择" class="filter-item" @change="selectChange">
+            <el-option
+              v-for="item in options"
+              :key="item.key"
+              :label="item.name"
+              :value="item.key"
+            />
+          </el-select>
+          <el-button
+            v-if="value"
+            class="filter-item"
+            icon="el-icon-plus"
+            style="margin-left: 10px;"
+            type="primary"
+            @click="handleAdd"
+          >
+            新增类型
+          </el-button>
+        </div>
+        <div>
+          <span style="display: inline-block">
+            <span class="filter-item">唯一标识:</span>
+            <el-input v-model="tableData.key" style="margin-left: 20px;width: 200px;" class="filter-item" />
+          </span>
+          <span style="display: inline-block">
+            <span class="filter-item">类型名称:</span>
+            <el-input v-model="tableData.name" style="margin-left: 20px;width: 200px;" class="filter-item" />
+          </span>
+          <span style="display: inline-block">
+            <span class="filter-item">描述:</span>
+            <el-input v-model="tableData.des" style="margin-left: 20px;width: 200px;" class="filter-item" />
+          </span>
+        </div>
+      </div>
+
+      <div class="filter-container" style="width:30%;float: right;text-align: right">
         <span v-if="!value" style="color: #cccccc;font-size: 10px;">(填写下方信息，保存即可新增)</span>
         <span v-if="value" style="color: #cccccc;font-size: 10px;">(编辑完成类型信息，点击保存)</span>
         <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="getList">
           保存
         </el-button>
       </div>
+
+      <el-table
+        :key="tableKey"
+        v-loading="listLoading"
+        :data="tableData.children"
+        border
+        fit
+        highlight-current-row
+      >
+        <el-table-column label="编号" align="center" min-width="150">
+          <template slot-scope="{row, $index}">
+            <span>{{ $index + 1 }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="名称" min-width="150" align="center">
+          <template slot-scope="{row}">
+            <!--          <span>{{ row.name }}</span>-->
+            <el-input v-model="row.name" />
+          </template>
+        </el-table-column>
+        <el-table-column label="描述" min-width="150" align="center">
+          <template slot-scope="{row}">
+            <!--          <span>{{ row.des }}</span>-->
+            <el-input v-model="row.des" />
+          </template>
+        </el-table-column>
+        <el-table-column label="是否启用" min-width="150" align="center">
+          <template slot-scope="{row}">
+            <el-button v-if="row.status === 1">禁用</el-button>
+            <el-button v-if="row.status === 0">启用</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column label="编辑" align="center" width="250" class-name="small-padding fixed-width">
+          <template slot-scope="{row,$index}">
+            <el-button type="danger" size="mini" @click="handleDelete(row)">
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div style="text-align: center">
+        <el-button style="margin: 10px auto;" type="primary" icon="el-icon-plus" @click="addRow">
+          添加数据列
+        </el-button>
+      </div>
     </div>
-    <div class="filter-container" style="float: right;margin-top: 10px;">
-      <span class="filter-item">唯一标识:</span>
-      <el-input v-model="tableData.key" style="margin-left: 20px;width: 200px;" class="filter-item"/>
-      <span class="filter-item">类型名称:</span>
-      <el-input v-model="tableData.name" style="margin-left: 20px;width: 200px;" class="filter-item"/>
-      <span class="filter-item">描述:</span>
-      <el-input v-model="tableData.des" style="margin-left: 20px;width: 200px;" class="filter-item"/>
-
-    </div>
-    <el-table
-
-      :key="tableKey"
-      v-loading="listLoading"
-      :data="tableData.children"
-      border
-      fit
-      highlight-current-row
-    >
-      <el-table-column label="编号" align="center" min-width="150">
-        <template slot-scope="{row, $index}">
-          <span>{{ $index + 1 }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="名称" min-width="150" align="center">
-        <template slot-scope="{row}">
-<!--          <span>{{ row.name }}</span>-->
-          <el-input v-model="row.name"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column label="描述" min-width="150" align="center">
-        <template slot-scope="{row}">
-<!--          <span>{{ row.des }}</span>-->
-          <el-input v-model="row.des"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column label="是否启用" min-width="150" align="center">
-        <template slot-scope="{row}">
-          <el-button v-if="row.status === 1">禁用</el-button>
-          <el-button v-if="row.status === 0">启用</el-button>
-        </template>
-      </el-table-column>
-      <el-table-column label="编辑" align="center" width="250" class-name="small-padding fixed-width">
-        <template slot-scope="{row,$index}">
-          <el-button type="danger" size="mini" @click="handleDelete(row)">
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div style="text-align: center">
-      <el-button style="margin: 10px auto;" type="primary" icon="el-icon-plus" @click="addRow">
-        添加数据列
-      </el-button>
-    </div>
-
-    <!--    <el-dialog width="600px" title="" :visible.sync="dialogFormVisible">-->
-    <!--      <el-form-->
-    <!--        ref="dataForm"-->
-    <!--        :rules="rules"-->
-    <!--        :model="temp"-->
-    <!--        label-position="right"-->
-    <!--        label-width="110px"-->
-    <!--        style="width: 400px; margin-left:50px;"-->
-    <!--      >-->
-    <!--        <el-form-item label="岗位编码：" prop="num">-->
-    <!--          <el-input v-model="temp.num" class="filter-item"/>-->
-
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="岗位名称：" prop="name">-->
-    <!--          <el-input v-model="temp.name" class="filter-item"/>-->
-    <!--        </el-form-item>-->
-
-    <!--        <el-form-item label="所属部门：" filterable prop="part">-->
-    <!--          <el-select v-model="temp.partId" class="filter-item" style="float: left;width: 100%;" placeholder="请选择">-->
-    <!--            <el-option-->
-    <!--              v-for="item in partOptions"-->
-    <!--              :key="item.id"-->
-    <!--              :label="item.name"-->
-    <!--              :value="item.id"-->
-    <!--            />-->
-    <!--          </el-select>-->
-    <!--        </el-form-item>-->
-
-    <!--        <el-form-item label="主要职责：">-->
-    <!--          <el-input v-model="temp.word" class="filter-item"/>-->
-    <!--        </el-form-item>-->
-    <!--      </el-form>-->
-    <!--      <div slot="footer" class="dialog-footer" style="text-align: center">-->
-    <!--        <el-button @click="dialogFormVisible = false">-->
-    <!--          取消-->
-    <!--        </el-button>-->
-    <!--        <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">-->
-    <!--          保存-->
-    <!--        </el-button>-->
-    <!--      </div>-->
-    <!--    </el-dialog>-->
-
   </div>
 </template>
 <script>
@@ -284,10 +278,46 @@
     }
   }
 </script>
-<style>
-  .download-button {
-    margin-bottom: 5px;
-    margin-top: 5px;
-    float: right
+<style lang="scss" scoped>
+  .analysis {
+    background-color: white;
+    flex: 1;
+
+    .title {
+      font-size: 16px;
+      font-weight: 500;
+      color: rgba(51, 51, 51, 1);
+      line-height: 35px;
+      margin-bottom: 8px;
+    }
+
+    .menu-2-box {
+      display: flex;
+      flex-wrap: wrap;
+      width: 100%;
+      padding-left: 2px;
+      padding-top: 2px;
+    }
+
+    .menu-2-item {
+      display: flex;
+      align-items: center;
+      color: #656565;
+      font-size: 12px;
+      width: 230px;
+      height: 101px;
+      background: rgb(255, 185, 129);
+      border-radius: 3px;
+      padding-left: 20px;
+      margin-right: 10px;
+      padding-top: 2px;
+      margin-bottom: 2px;
+      cursor: pointer;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+
+      .text {
+        margin-left: 16px;
+      }
+    }
   }
 </style>
