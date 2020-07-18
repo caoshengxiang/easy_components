18e8eea16f9aaeb1c75e0e1daf8026ac917cb1e4
yangzailin <908869887@qq.com>
2020-07-18 11:32:57 +0800

    还原老姚的代码

--- src/api/modules/workflow.js
+++ src/api/modules/workflow.js
@@ -8,7 +8,7 @@ export default {
     return request.get(`approvalConfig/${id}`)
   },
   save(params){
-    return request.put(`approvalConfig/definition/${params.id}`, params)
+    return request.put(`approvalConfig/${params.id}`, params)
   },
   add(params){
     return request.post(`approvalConfig`, params)
