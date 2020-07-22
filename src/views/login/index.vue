350b0f33bbde6bab3f64792a0f8ba3ebc519a97f
yaozhengwei <cto.china@qq.com>
2020-07-22 09:22:55 +0800

    chore:登录页面样式调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,14 +9,14 @@
       label-position="left"
     >
 
-      <div class="title-container">
-        <h3 class="title">xxxxx</h3>
-      </div>
+      <el-form-item>
+        <div class="title-container">
+          <h3 class="title">职业院校综合管理及内部质量诊断与改进平台
+          </h3>
+        </div>
+      </el-form-item>
 
       <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user"/>
-        </span>
         <el-input
           ref="username"
           v-model="loginForm.account"
@@ -25,40 +25,44 @@
           type="text"
           tabindex="1"
           autocomplete="on"
-        />
+        >
+          <i slot="prefix" class="el-input__icon el-icon-user"></i>
+        </el-input>
       </el-form-item>
 
-      <el-tooltip v-model="capsTooltip" content="大写字母" placement="right" manual>
-        <el-form-item prop="password">
-          <span class="svg-container">
-            <svg-icon icon-class="password"/>
-          </span>
-          <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.pwd"
-            :type="passwordType"
-            placeholder="请输入密码"
-            name="password"
-            tabindex="2"
-            autocomplete="on"
-            @keyup.native="checkCapslock"
-            @blur="capsTooltip = false"
-            @keyup.enter.native="handleLogin"
-          />
-          <span class="show-pwd" @click="showPwd">
+      <el-form-item prop="password">
+        <el-input
+          :key="passwordType"
+          ref="password"
+          v-model="loginForm.pwd"
+          :type="passwordType"
+          placeholder="请输入密码"
+          name="password"
+          tabindex="2"
+          autocomplete="on"
+          @keyup.native="checkCapslock"
+          @blur="capsTooltip = false"
+          @keyup.enter.native="handleLogin"
+        >
+          <i slot="prefix" class="el-input__icon el-icon-unlock"></i>
+        </el-input>
+        <span class="show-pwd" @click="showPwd">
             <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>
           </span>
-        </el-form-item>
-      </el-tooltip>
+      </el-form-item>
 
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width:100%;margin-bottom:30px;padding: 13px 0;"
-        @click.native.prevent="handleLogin"
-      >登录
-      </el-button>
+      <el-form-item>
+        <div class="login-btn">
+          <el-button
+            round
+            :loading="loading"
+            type="primary"
+            style="width:100%;margin-bottom:30px;padding: 13px 0;"
+            @click.native.prevent="handleLogin"
+          >登录
+          </el-button>
+        </div>
+      </el-form-item>
     </el-form>
   </div>
 </template>
@@ -205,76 +209,36 @@
 </script>
 
 <style lang="scss" scoped>
-  /* 修复input 背景不协调 和光标变色 */
-  /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-  $bg: #283443;
-  $light_gray: #fff;
-  $cursor: #fff;
-
-  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-    .login-container .el-input input {
-      color: $cursor;
-    }
-  }
-
-  /* reset element-ui css */
-  .login-container {
-    .el-input {
-      display: inline-block;
-      height: 47px;
-      width: 85%;
-
-      input {
-        background: transparent;
-        border: 0;
-        -webkit-appearance: none;
-        border-radius: 0px;
-        padding: 12px 5px 12px 15px;
-        color: $light_gray;
-        height: 47px;
-        caret-color: $cursor;
-
-        &:-webkit-autofill {
-          box-shadow: 0 0 0 1000px $bg inset !important;
-          -webkit-text-fill-color: $cursor !important;
-        }
-      }
-    }
-
-    .el-form-item {
-      border: 1px solid rgba(255, 255, 255, 0.1);
-      background: rgba(0, 0, 0, 0.2);
-      border-radius: 5px;
-      color: #454545;
-    }
-  }
-</style>
-
-<style lang="scss" scoped>
   $bg: #ffffff;
   $dark_gray: #ffffff;
   $light_gray: #eee;
+  $assets: '~@/assets/';
 
   .login-container {
-    min-height: 100%;
-    width: 100%;
-    background-color: $bg;
-    /*background-image: url("./bg.jpg");*/
+    background: url($assets + "images/logo-bg.png") 0 0 no-repeat;
     background-size: 100% 100%;
-    overflow: hidden;
-
+    position: absolute;
+    top:0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     .login-form {
-      position: relative;
-      left: 300px;
-      top: 250px;
-      width: 520px;
-      max-width: 100%;
-      padding: 35px;
-      margin: 0 auto;
-      overflow: hidden;
-      /*border: 1px solid #9a98a1;*/
-      background-color: rgba(255, 255, 255, 0.2);
+      position: absolute;
+      width: 38%;
+      height: 63%;
+      background-color: #fff;
+      border-radius: 20px;
+      padding: 80px;
+      box-shadow:0px 0px  10px 5px #ddd;
+      .login-btn{
+        padding: 30px 0;
+      }
+      .el-form-item{
+        margin-bottom: 30px;
+      }
     }
 
     .tips {
@@ -302,9 +266,7 @@
 
       .title {
         font-size: 30px;
-        color: #157ddd;
         margin: 0 auto 40px auto;
-        text-align: center;
         font-weight: bold;
       }
     }
@@ -330,5 +292,17 @@
         display: none;
       }
     }
+  }
+</style>
+<style scoped>
+  .login-form >>>.el-input input{
+    background:rgba(247,247,247,1);
+  }
+  .login-form >>>.el-input--small .el-input__inner{
+    height: 40px;
+    line-height: 40px;
+  }
+  .login-form >>> .el-input__icon{
+    font-size: 18px;
   }
 </style>
