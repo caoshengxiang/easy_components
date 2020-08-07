a2a56153604fca439f38438584b633ab7a0b435b
yangzailin <908869887@qq.com>
2020-08-07 15:11:37 +0800

    bug fix

--- src/views/workflow/task/List.vue
+++ src/views/workflow/task/List.vue
@@ -158,13 +158,20 @@
         let that = this
         that.dialogFormVisible = true
         that.batchAudit  =1;
+        that.temp = {}
         that.$nextTick(() => {
           that.$refs['temp'].clearValidate()
         })
       },
       cancleAudit(){
+        let that = this
         this.dialogFormVisible = false
         this.batchAudit  = 0;
+
+        that.temp = {}
+        that.$nextTick(() => {
+          that.$refs['temp'].clearValidate()
+        })
       },
       handleSelectionChange(val) {
         this.multipleSelection = val;
