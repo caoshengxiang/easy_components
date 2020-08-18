f1b586c0a1d19c44f0a4be745f132574e83fdcff
yangzailin <908869887@qq.com>
2020-08-18 15:30:12 +0800

    通知书成绩

--- src/views/moralManage/conductScore/index.vue
+++ src/views/moralManage/conductScore/index.vue
@@ -1,6 +1,11 @@
 <template>
   <common-layout>
-    caoxingfen
+    <div>
+      <iframe
+        src='https://view.officeapps.live.com/op/view.aspx?src=http://cdpta.cdhrss.chengdu.gov.cn/frt/frtuploadfile/uploadaffix/link/bulletin2020/20200713215944137.docx'
+        style="overflow: auto; position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: 100%; height: 100%"
+      ></iframe>
+    </div>
   </common-layout>
 </template>
 
