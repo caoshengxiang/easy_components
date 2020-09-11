8539455d8e3a7a25f9a26413095635e73840f496
秦修骅 <xiuhua.qin@changhong.com>
2020-09-11 10:15:54 +0800

    奖惩下载预览

--- src/views/rewardsAndPunishments/info/detail.vue
+++ src/views/rewardsAndPunishments/info/detail.vue
@@ -159,6 +159,8 @@
   import fileUpload from '@/components/FileUpload'
   import YDetailPageLayout from './components/YDetailPageLayout'
   import printPdf from "./printPdf";
+  import html2canvas from "html2canvas";
+  import jsPDF from "jspdf";
 
   export default {
     name: 'cancelDetailForm',
@@ -222,7 +224,8 @@
         AllEnum:{},
         loading:false,
         originData:{},
-        loadComplete: false
+        loadComplete: false,
+        urlStr: '',
       }
     },
     watch: {
@@ -361,7 +364,49 @@
         })
       },
       printInfo() {
-        printPdf('#rewardsAndPunishmentsDetailInfo', '奖惩')
+        // console.log(printPdf('#rewardsAndPunishmentsDetailInfo', '奖惩'))
+        // console.log(psf)
+        html2canvas(document.querySelector('#rewardsAndPunishmentsDetailInfo'), {
+          // 背景设为白色（默认为黑色）
+          background: '#fff',
+          dpi: 400, // 导出pdf清晰度,DPI越低，扫描的清晰度越低
+          scale: 3
+        }).then(canvas => {
+          // document.body.appendChild(canvas)
+          const contentWidth = canvas.width
+          const contentHeight = canvas.height
+          // 一页pdf显示html页面生成的canvas高度;
+          var pageHeight = contentWidth / 592.28 * 841.89
+          // 未生成pdf的html页面高度
+          var leftHeight = contentHeight
+          // pdf页面偏移
+          var position = 0
+          // html页面生成的canvas在pdf中图片的宽高（a4纸的尺寸[595.28,841.89]）
+          var imgWidth = 595.28
+          var imgHeight = 592.28 / contentWidth * contentHeight
+          var pageData = canvas.toDataURL('image/jpeg', 1.0)
+          // eslint-disable-next-line
+          var pdf = new jsPDF('', 'pt', 'a4')
+          // 有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+          // 当内容未超过pdf一页显示的范围，无需分页
+          if (leftHeight < pageHeight) {
+            pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
+          } else {
+            while (leftHeight > 0) {
+              pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+              leftHeight -= pageHeight
+              position -= 841.89
+              // 避免添加空白页
+              if (leftHeight > 0) {
+                pdf.addPage()
+              }
+            }
+          }
+          // pdf.save(`${name}.pdf`)
+          this.urlStr = pdf.output('dataurlstring')
+          localStorage.setItem('r-print-url', this.urlStr);
+          window.open('/pdf/web/viewer.html')
+        })
       },
       getDetail() {
         const that = this;
@@ -442,6 +487,9 @@
           }
         })
       },
+      print() {
+        document.getElementById('printF').contentWindow.print()
+      }
     }
   }
 </script>
