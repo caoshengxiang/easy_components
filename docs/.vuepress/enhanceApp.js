9d094129504aa1bbb4cea8ba84a4de25828c864f
caoshengxiang <1104729815@qq.com>
2020-09-26 21:54:50 +0800

    2

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,4 +1,3 @@
-
 /**
  * 扩展 VuePress 应用
  */
@@ -7,17 +6,19 @@ import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 //vue代码高亮显示库 VueHighlightJS
-import VueHighlightJS from 'vue-highlight.js';
+import VueHighlightJS from 'vue-highlight.js'
 //这里样式我选择的是atom-one-light；样式更多选择可以参见 https://highlightjs.org/static/demo/ 里的styles ；打不开直接到node_module找
 //注意： 代码块的背景色 还是由官方设置的 $codeBgColor 决定的
-import 'highlight.js/styles/atom-one-light.css';
+import 'highlight.js/styles/atom-one-light.css'
 // import 'highlight.js/styles/atom-one-dark-reasonable.css';
 
 import vDialogs from 'v-dialogs' //弹窗组件
 
 import cuCustom from '../../public/colorui/components/cu-custom'
-import '../../public/colorui/coloruipx.less' // 样式库
-import '../../public/colorui/icon.css'; // 图标
+
+import '../../public/colorui/coloruivw.less'
+
+import '../../public/colorui/icon.css' // 图标
 
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
